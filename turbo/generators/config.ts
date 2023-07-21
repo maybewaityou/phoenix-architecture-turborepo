55eaa1a6ca8253fa682f7324accce34aaeaca620
MeePwn <maybewaityou@gmail.com>
2023-07-21 15:16:02 +0800

    first commit

--- turbo/generators/config.ts
+++ turbo/generators/config.ts
@@ -23,6 +23,11 @@ export default function generator(plop: PlopTypes.NodePlopAPI): void {
       },
       {
         type: 'add',
+        path: 'packages/{{name}}/LICENSE',
+        templateFile: 'templates/LICENSE'
+      },
+      {
+        type: 'add',
         path: 'packages/{{name}}/package.json',
         templateFile: 'templates/package.json.hbs'
       },
