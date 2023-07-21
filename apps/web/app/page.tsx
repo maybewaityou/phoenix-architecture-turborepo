bbfd6d52562b131fecce83c4bc78dadcef913b70
MeePwn <maybewaityou@gmail.com>
2023-07-21 10:25:40 +0800

    first commit

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -15,7 +15,6 @@ export default function Page() {
   const [state, setState] = useState(0)
 
   async function handleClick() {
-    console.log('=========', state)
     await delay(1000)
     setState(state + 1)
   }
